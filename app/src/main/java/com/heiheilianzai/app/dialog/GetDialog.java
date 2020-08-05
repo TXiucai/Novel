6025e538ff85fdbd1bafa4e1b676f9574ebbe0e3
Kozen <Kozen@qqc88.net>
2020-08-05 21:24:43 +0800

    1、格式化GetDialog代码,优化其中操作提示对话框
    Signed-off-by: Kozen <Kozen@qqc88.net>
    Change-Id: I8a4fd0de120c2a33d51040cc2b5880c7789b1ffc

--- app/src/main/java/com/heiheilianzai/app/dialog/GetDialog.java
+++ app/src/main/java/com/heiheilianzai/app/dialog/GetDialog.java
@@ -37,89 +37,46 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 /**
+ * 系统自带 操作提示弹框（确定，取消）、时间选择弹框
  * Created by Administrator on 2018/7/6.
  */
-
 public class GetDialog {
-
-
-    public interface GetEditTextDialogInterface {
-        void getText(String text);
-    }
-
-    static PopupWindow popupWindow;
-
-    public static void getEditTextDialog(final Activity activity, View v, String title, String hint, final GetEditTextDialogInterface getEditTextDialogInterface) {
-
-     /*   View view = LayoutInflater.from(activity).inflate(R.layout.dialog_getedittext, null);
-         popupWindow = new PopupWindow(view, ImageUtil.dp2px(activity, 300), ImageUtil.dp2px(activity, 230), true);
-        Button dialog_getedittext_cancle = (Button) view.findViewById(R.id.dialog_getedittext_cancle);
-        Button dialog_getedittext_ok = (Button) view.findViewById(R.id.dialog_getedittext_ok);
-        TextView dialog_getedittext_title = (TextView) view.findViewById(R.id.dialog_getedittext_title);
-        final EditText dialog_getedittext_edittext1 = (EditText) view.findViewById(R.id.dialog_getedittext_edittext1);
-        dialog_getedittext_title.setText(title);
-        dialog_getedittext_edittext1.setHint(hint);
-        dialog_getedittext_cancle.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                popupWindow.dismiss();
-            }
-        });
-        dialog_getedittext_ok.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String string = dialog_getedittext_edittext1.getText().toString();
-                if (string.length() > 0) {
-                    getEditTextDialogInterface.getText(string);
-                    popupWindow.dismiss();
-                } else {
-
-                    MyShow.myToash(activity, "输入不能为空");
-                }
-
-
-            }
-        });
-        view.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                popupWindow.dismiss();
-                return false;
-            }
-        });
-
-
-        popupWindow.setFocusable(true);
-        popupWindow.setOutsideTouchable(true);
-        popupWindow.showAtLocation(v, Gravity.CENTER, 0, 0);*/
-
-
-    }
-
+    private static final String CHECK_OP_NO_THROW = "checkOpNoThrow";
+    private static final String OP_POST_NOTIFICATION = "OP_POST_NOTIFICATION";
 
     public interface IsOperationInterface {
         void isOperation();
     }
 
     public static void IsOperation(final Activity activity, String title, String suretext, final IsOperationInterface isOperationInterface) {
+        IsOperationPositiveNegative(activity, title, suretext, isOperationInterface, true, true);
+    }
+
+    public static void IsOperationPositive(final Activity activity, String title, String suretext, final IsOperationInterface isOperationInterface) {
+        IsOperationPositiveNegative(activity, title, suretext, isOperationInterface, true, false);
+    }
+
+    public static void IsOperationPositiveNegative(final Activity activity, String title, String suretext, final IsOperationInterface isOperationInterface, boolean isPositive, boolean isNegative) {
         if (activity == null) {
             return;
         }
-        Dialog dialog = new AlertDialog.Builder(activity)
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity)
                 .setTitle(title)
-                .setMessage(suretext)
-                .setPositiveButton(LanguageUtil.getString(activity, R.string.public_sure), new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        isOperationInterface.isOperation();
-
-
-                    }
-                })
-                .setNegativeButton(LanguageUtil.getString(activity, R.string.splashactivity_cancle), new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-
-                    }
-                })
-                .create();
-        dialog.show();
+                .setMessage(suretext);
+        if (isPositive) {
+            builder.setPositiveButton(LanguageUtil.getString(activity, R.string.public_sure), new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    isOperationInterface.isOperation();
+                }
+            });
+        }
+        if (isNegative) {
+            builder.setNegativeButton(LanguageUtil.getString(activity, R.string.splashactivity_cancle), new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                }
+            });
+        }
+        builder.create().show();
     }
 
     public interface GetTimeDialogInterface {
@@ -136,7 +93,6 @@ public class GetDialog {
                     }
                 },
                 calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DATE));
-
         DatePicker datePicker = datePickerDialog.getDatePicker();
         datePickerDialog.setTitle("选择查询日期");
         try {
@@ -147,54 +103,32 @@ public class GetDialog {
         } //设置最大日期
         datePicker.setMaxDate(System.currentTimeMillis());
         datePickerDialog.show();
-
     }
 
-
-
-
-
-
-
-        private static final String CHECK_OP_NO_THROW = "checkOpNoThrow";
-        private static final String OP_POST_NOTIFICATION = "OP_POST_NOTIFICATION";
-
-        @TargetApi(Build.VERSION_CODES.KITKAT)
-        public static boolean isNotificationEnabled(Context context) {
-
-            AppOpsManager mAppOps = (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);
-
-            ApplicationInfo appInfo = context.getApplicationInfo();
-
-            String pkg = context.getApplicationContext().getPackageName();
-
-            int uid = appInfo.uid;
-
-            Class appOpsClass = null; /* Context.APP_OPS_MANAGER */
-
-            try {
-
-                appOpsClass = Class.forName(AppOpsManager.class.getName());
-
-                Method checkOpNoThrowMethod = appOpsClass.getMethod(CHECK_OP_NO_THROW, Integer.TYPE, Integer.TYPE, String.class);
-
-                Field opPostNotificationValue = appOpsClass.getDeclaredField(OP_POST_NOTIFICATION);
-                int value = (int)opPostNotificationValue.get(Integer.class);
-
-                return ((int)checkOpNoThrowMethod.invoke(mAppOps,value, uid, pkg) == AppOpsManager.MODE_ALLOWED);
-
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            } catch (NoSuchMethodException e) {
-                e.printStackTrace();
-            } catch (NoSuchFieldException e) {
-                e.printStackTrace();
-            } catch (InvocationTargetException e) {
-                e.printStackTrace();
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
-            return false;
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    public static boolean isNotificationEnabled(Context context) {
+        AppOpsManager mAppOps = (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);
+        ApplicationInfo appInfo = context.getApplicationInfo();
+        String pkg = context.getApplicationContext().getPackageName();
+        int uid = appInfo.uid;
+        Class appOpsClass = null;
+        try {
+            appOpsClass = Class.forName(AppOpsManager.class.getName());
+            Method checkOpNoThrowMethod = appOpsClass.getMethod(CHECK_OP_NO_THROW, Integer.TYPE, Integer.TYPE, String.class);
+            Field opPostNotificationValue = appOpsClass.getDeclaredField(OP_POST_NOTIFICATION);
+            int value = (int) opPostNotificationValue.get(Integer.class);
+            return ((int) checkOpNoThrowMethod.invoke(mAppOps, value, uid, pkg) == AppOpsManager.MODE_ALLOWED);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
         }
-
+        return false;
+    }
 }
