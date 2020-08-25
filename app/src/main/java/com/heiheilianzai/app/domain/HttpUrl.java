207b5fbec741bc0f991156e033e58d8317298d09
Kozen <Kozen@qqc88.net>
2020-08-25 09:56:29 +0800

    1、格式化HttpUrl,替换Util.UTFTF_8引入。
    Signed-off-by: Kozen <Kozen@qqc88.net>
    Change-Id: Ib8375f13b68aec1f2c479bf4ab9dc2da2ca8ee70

--- app/src/main/java/com/heiheilianzai/app/domain/HttpUrl.java
+++ app/src/main/java/com/heiheilianzai/app/domain/HttpUrl.java
@@ -1,45 +1,13 @@
 package com.heiheilianzai.app.domain;
-/*
- * Copyright (C) 2015 Square, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
-
-import android.support.annotation.Nullable;
-
-//import com.squareup.okhttp.internal.Util;
-
-import java.net.InetAddress;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
 
-import okhttp3.internal.Util;
+import cn.jmessage.support.okhttp3.internal.Util;
 import okio.Buffer;
 
-
 public final class HttpUrl {
-    private static final char[] HEX_DIGITS =
-            {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
+    private static final char[] HEX_DIGITS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
     /**
      * Returns a substring of {@code input} on the range {@code [pos..limit)} with the following
@@ -53,14 +21,12 @@ public final class HttpUrl {
      * </ul>
      *
      * @param alreadyEncoded true to leave '%' as-is; false to convert it to '%25'.
-     * @param strict true to encode '%' if it is not the prefix of a valid percent encoding.
-     * @param plusIsSpace true to encode '+' as "%2B" if it is not already encoded.
-     * @param asciiOnly true to encode all non-ASCII codepoints.
-     * @param charset which charset to use, null equals UTF-8.
+     * @param strict         true to encode '%' if it is not the prefix of a valid percent encoding.
+     * @param plusIsSpace    true to encode '+' as "%2B" if it is not already encoded.
+     * @param asciiOnly      true to encode all non-ASCII codepoints.
+     * @param charset        which charset to use, null equals UTF-8.
      */
-    static String canonicalize(String input, int pos, int limit, String encodeSet,
-                               boolean alreadyEncoded, boolean strict, boolean plusIsSpace, boolean asciiOnly,
-                               Charset charset) {
+    static String canonicalize(String input, int pos, int limit, String encodeSet, boolean alreadyEncoded, boolean strict, boolean plusIsSpace, boolean asciiOnly, Charset charset) {
         int codePoint;
         for (int i = pos; i < limit; i += Character.charCount(codePoint)) {
             codePoint = input.codePointAt(i);
@@ -68,9 +34,7 @@ public final class HttpUrl {
                     || codePoint == 0x7f
                     || codePoint >= 0x80 && asciiOnly
                     || encodeSet.indexOf(codePoint) != -1
-                //    || codePoint == '%' && (!alreadyEncoded || strict && !percentEncoded(input, i, limit))
                     || codePoint == '+' && plusIsSpace) {
-                // Slow path: the character at i requires encoding!
                 Buffer out = new Buffer();
                 out.writeUtf8(input, pos, i);
                 canonicalize(out, input, i, limit, encodeSet, alreadyEncoded, strict, plusIsSpace,
@@ -78,40 +42,30 @@ public final class HttpUrl {
                 return out.readUtf8();
             }
         }
-
-        // Fast path: no characters in [pos..limit) required encoding.
         return input.substring(pos, limit);
     }
 
-    static void canonicalize(Buffer out, String input, int pos, int limit, String encodeSet,
-                             boolean alreadyEncoded, boolean strict, boolean plusIsSpace, boolean asciiOnly,
-                             Charset charset) {
-        Buffer encodedCharBuffer = null; // Lazily allocated.
+    static void canonicalize(Buffer out, String input, int pos, int limit, String encodeSet, boolean alreadyEncoded, boolean strict, boolean plusIsSpace, boolean asciiOnly, Charset charset) {
+        Buffer encodedCharBuffer = null;
         int codePoint;
         for (int i = pos; i < limit; i += Character.charCount(codePoint)) {
             codePoint = input.codePointAt(i);
             if (alreadyEncoded
                     && (codePoint == '\t' || codePoint == '\n' || codePoint == '\f' || codePoint == '\r')) {
-                // Skip this character.
             } else if (codePoint == '+' && plusIsSpace) {
-                // Encode '+' as '%2B' since we permit ' ' to be encoded as either '+' or '%20'.
                 out.writeUtf8(alreadyEncoded ? "+" : "%2B");
             } else if (codePoint < 0x20
                     || codePoint == 0x7f
                     || codePoint >= 0x80 && asciiOnly
-                    || encodeSet.indexOf(codePoint) != -1){
-                 //   || codePoint == '%' && (!alreadyEncoded || strict && !percentEncoded(input, i, limit))) {
-                // Percent encode this character.
+                    || encodeSet.indexOf(codePoint) != -1) {
                 if (encodedCharBuffer == null) {
                     encodedCharBuffer = new Buffer();
                 }
-
                 if (charset == null || charset.equals(Util.UTF_8)) {
                     encodedCharBuffer.writeUtf8CodePoint(codePoint);
                 } else {
                     encodedCharBuffer.writeString(input, i, i + Character.charCount(codePoint), charset);
                 }
-
                 while (!encodedCharBuffer.exhausted()) {
                     int b = encodedCharBuffer.readByte() & 0xff;
                     out.writeByte('%');
@@ -119,23 +73,16 @@ public final class HttpUrl {
                     out.writeByte(HEX_DIGITS[b & 0xf]);
                 }
             } else {
-                // This character doesn't need encoding. Just copy it over.
                 out.writeUtf8CodePoint(codePoint);
             }
         }
     }
 
-    static String canonicalize(String input, String encodeSet, boolean alreadyEncoded, boolean strict,
-                               boolean plusIsSpace, boolean asciiOnly, Charset charset) {
-        return canonicalize(
-                input, 0, input.length(), encodeSet, alreadyEncoded, strict, plusIsSpace, asciiOnly,
-                charset);
+    static String canonicalize(String input, String encodeSet, boolean alreadyEncoded, boolean strict, boolean plusIsSpace, boolean asciiOnly, Charset charset) {
+        return canonicalize(input, 0, input.length(), encodeSet, alreadyEncoded, strict, plusIsSpace, asciiOnly, charset);
     }
 
-    static String canonicalize(String input, String encodeSet, boolean alreadyEncoded, boolean strict,
-                               boolean plusIsSpace, boolean asciiOnly) {
-        return canonicalize(
-                input, 0, input.length(), encodeSet, alreadyEncoded, strict, plusIsSpace, asciiOnly, null);
+    static String canonicalize(String input, String encodeSet, boolean alreadyEncoded, boolean strict, boolean plusIsSpace, boolean asciiOnly) {
+        return canonicalize(input, 0, input.length(), encodeSet, alreadyEncoded, strict, plusIsSpace, asciiOnly, null);
     }
 }
-
