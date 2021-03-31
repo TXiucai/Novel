b11a62e9e8b3a5093575050200755c88d9defc0b
jaki <Jaki@qqc88.net>
2021-03-31 18:40:17 +0800

    图片选择器，添加，删除
    Change-Id: Ibf37c7dafa14290a9586d1999b2110b285d74f44

---
+++ app/src/main/java/com/heiheilianzai/app/adapter/ImagePickerAdapter.java
@@ -0,0 +1,119 @@
+package com.heiheilianzai.app.adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.heiheilianzai.app.R;
+import com.heiheilianzai.app.ui.activity.FeedBackActivity;
+import com.lzy.imagepicker.ImagePicker;
+import com.lzy.imagepicker.bean.ImageItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ImagePickerAdapter extends RecyclerView.Adapter<ImagePickerAdapter.SelectedPicViewHolder> {
+    private int maxImgCount;
+    private Context mContext;
+    private List<ImageItem> mData;
+    private LayoutInflater mInflater;
+    private OnRecyclerViewItemClickListener listener;
+    private boolean isAdded;   //是否额外添加了最后一个图片
+
+    public interface OnRecyclerViewItemClickListener {
+        void onItemClick(View view, int position);
+
+        void onItemDeleteClick(View view, int position);
+    }
+
+    public void setOnItemClickListener(OnRecyclerViewItemClickListener listener) {
+        this.listener = listener;
+    }
+
+    public void setImages(List<ImageItem> data) {
+        mData = new ArrayList<>(data);
+        if (getItemCount() < maxImgCount) {
+            mData.add(new ImageItem());
+            isAdded = true;
+        } else {
+            isAdded = false;
+        }
+        notifyDataSetChanged();
+    }
+
+    public List<ImageItem> getImages() {
+        //由于图片未选满时，最后一张显示添加图片，因此这个方法返回真正的已选图片
+        if (isAdded) return new ArrayList<>(mData.subList(0, mData.size() - 1));
+        else return mData;
+    }
+
+    public ImagePickerAdapter(Context mContext, List<ImageItem> data, int maxImgCount) {
+        this.mContext = mContext;
+        this.maxImgCount = maxImgCount;
+        this.mInflater = LayoutInflater.from(mContext);
+        setImages(data);
+    }
+
+    @Override
+    public SelectedPicViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new SelectedPicViewHolder(mInflater.inflate(R.layout.list_item_image, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(SelectedPicViewHolder holder, int position) {
+        holder.bind(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public class SelectedPicViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private ImageView iv_img;
+        private int clickPosition;
+        private ImageView iv_delete;
+
+        public SelectedPicViewHolder(View itemView) {
+            super(itemView);
+            iv_img = (ImageView) itemView.findViewById(R.id.iv_img);
+            iv_delete = (ImageView) itemView.findViewById(R.id.iv_delete);
+        }
+
+        public void bind(int position) {
+            //设置条目的点击事件
+            iv_img.setOnClickListener(this);
+            iv_delete.setOnClickListener(this);
+            //根据条目位置设置图片
+            ImageItem item = mData.get(position);
+            if (isAdded && position == getItemCount() - 1) {
+                iv_img.setImageResource(R.mipmap.add);
+                clickPosition = FeedBackActivity.IMAGE_ITEM_ADD;
+                iv_delete.setVisibility(View.GONE);
+            } else {
+                ImagePicker.getInstance().getImageLoader().displayImage((Activity) mContext, item.path, iv_img, 0, 0);
+                clickPosition = position;
+                iv_delete.setVisibility(View.VISIBLE);
+            }
+        }
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.iv_img:
+                    if (listener != null) listener.onItemClick(v, clickPosition);
+                    break;
+                case R.id.iv_delete:
+                    if (listener != null) listener.onItemDeleteClick(v, clickPosition);
+                    break;
+            }
+
+        }
+    }
+}
--- app/src/main/java/com/heiheilianzai/app/ui/activity/FeedBackActivity.java
+++ app/src/main/java/com/heiheilianzai/app/ui/activity/FeedBackActivity.java
@@ -1,5 +1,6 @@
 package com.heiheilianzai.app.ui.activity;
 
+import android.content.Intent;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
@@ -15,16 +16,23 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.google.gson.Gson;
 import com.heiheilianzai.app.R;
 import com.heiheilianzai.app.adapter.FeedBackTypeAdapter;
+import com.heiheilianzai.app.adapter.ImagePickerAdapter;
 import com.heiheilianzai.app.base.BaseActivity;
 import com.heiheilianzai.app.callback.ShowTitle;
 import com.heiheilianzai.app.component.http.ReaderParams;
 import com.heiheilianzai.app.component.task.MainHttpTask;
 import com.heiheilianzai.app.constant.ReaderConfig;
 import com.heiheilianzai.app.model.ComplaitTypeBean;
+import com.heiheilianzai.app.utils.GlideImageLoader;
 import com.heiheilianzai.app.utils.HttpUtils;
 import com.heiheilianzai.app.utils.LanguageUtil;
 import com.heiheilianzai.app.utils.MyToash;
+import com.lzy.imagepicker.ImagePicker;
+import com.lzy.imagepicker.bean.ImageItem;
+import com.lzy.imagepicker.ui.ImageGridActivity;
+import com.lzy.imagepicker.view.CropImageView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
@@ -34,7 +42,7 @@ import butterknife.BindView;
  * 个人中心-意见反馈
  * Created by scb on 2018/7/14.
  */
-public class FeedBackActivity extends BaseActivity implements ShowTitle {
+public class FeedBackActivity extends BaseActivity implements ShowTitle, ImagePickerAdapter.OnRecyclerViewItemClickListener {
     private final String TAG = FeedBackActivity.class.getSimpleName();
     /**
      * 意见内容
@@ -53,8 +61,15 @@ public class FeedBackActivity extends BaseActivity implements ShowTitle {
 
     @BindView(R.id.tabRecycler)
     RecyclerView mTbRecycleView;
+    @BindView(R.id.recyclerView)
+    RecyclerView mPicRecycleView;
 
     private FeedBackTypeAdapter mTypeAdapter;
+    public static final int IMAGE_ITEM_ADD = -1;
+    public static final int REQUEST_CODE_SELECT = 100;
+    private ImagePickerAdapter mImagePickerAdapter;
+    private ArrayList<ImageItem> selImageList; //当前选择的所有图片
+    private int maxImgCount = 6;               //允许选择图片最大数
 
     @Override
     public int initContentView() {
@@ -65,6 +80,7 @@ public class FeedBackActivity extends BaseActivity implements ShowTitle {
     @Override
     public void initView() {
         initTitleBarView(LanguageUtil.getString(this, R.string.FeedBackActivity_title));
+        initImagePicker();
         mEditTextContent = findViewById(R.id.activity_feedback_content);
         mEditTextContent.addTextChangedListener(new TextWatcher() {
             @Override
@@ -96,6 +112,14 @@ public class FeedBackActivity extends BaseActivity implements ShowTitle {
         mTbRecycleView.setLayoutManager(new GridLayoutManager(this, 3, GridLayoutManager.VERTICAL, false));
         mTypeAdapter = new FeedBackTypeAdapter(this);
         mTbRecycleView.setAdapter(mTypeAdapter);
+
+        //图片
+        selImageList = new ArrayList<>();
+        mImagePickerAdapter = new ImagePickerAdapter(this, selImageList, maxImgCount);
+        mImagePickerAdapter.setOnItemClickListener(this);
+        mPicRecycleView.setLayoutManager(new GridLayoutManager(this, 3));
+        mPicRecycleView.setHasFixedSize(true);
+        mPicRecycleView.setAdapter(mImagePickerAdapter);
     }
 
     @Override
@@ -164,19 +188,64 @@ public class FeedBackActivity extends BaseActivity implements ShowTitle {
 
     }
 
+    private void initImagePicker() {
+        ImagePicker imagePicker = ImagePicker.getInstance();
+        imagePicker.setImageLoader(new GlideImageLoader());   //设置图片加载器
+        imagePicker.setShowCamera(false);                      //显示拍照按钮
+        imagePicker.setCrop(true);                           //允许裁剪（单选才有效）
+        imagePicker.setSaveRectangle(true);                   //是否按矩形区域保存
+        imagePicker.setSelectLimit(maxImgCount);              //选中数量限制
+        imagePicker.setStyle(CropImageView.Style.RECTANGLE);  //裁剪框的形状
+        imagePicker.setFocusWidth(800);                       //裁剪框的宽度。单位像素（圆形自动取宽高最小值）
+        imagePicker.setFocusHeight(800);                      //裁剪框的高度。单位像素（圆形自动取宽高最小值）
+        imagePicker.setOutPutX(1000);                         //保存文件的宽度。单位像素
+        imagePicker.setOutPutY(1000);                         //保存文件的高度。单位像素
+    }
+
     @Override
     public void initTitleBarView(String text) {
         LinearLayout mBack;
         TextView mTitle;
         mBack = findViewById(R.id.titlebar_back);
         mTitle = findViewById(R.id.titlebar_text);
-        mBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+        mBack.setOnClickListener(v -> finish());
         mTitle.setText(text);
     }
 
+    @Override
+    public void onItemClick(View view, int position) {
+        switch (position) {
+            case IMAGE_ITEM_ADD:
+                //打开选择,本次允许选择的数量
+                ImagePicker.getInstance().setSelectLimit(maxImgCount - selImageList.size());
+                Intent intent1 = new Intent(FeedBackActivity.this, ImageGridActivity.class);
+                startActivityForResult(intent1, REQUEST_CODE_SELECT);
+                break;
+        }
+    }
+
+    @Override
+    public void onItemDeleteClick(View view, int position) {
+        selImageList.remove(position);
+        if (selImageList != null) {
+            mImagePickerAdapter.setImages(selImageList);
+        }
+    }
+
+    ArrayList<ImageItem> images = null;
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == ImagePicker.RESULT_CODE_ITEMS) {
+            //添加图片返回
+            if (data != null && requestCode == REQUEST_CODE_SELECT) {
+                images = (ArrayList<ImageItem>) data.getSerializableExtra(ImagePicker.EXTRA_RESULT_ITEMS);
+                if (images != null) {
+                    selImageList.addAll(images);
+                    mImagePickerAdapter.setImages(selImageList);
+                }
+            }
+        }
+    }
 }
